7a88e597b2449d3c55117c55c5d774ed1e4d0cb9
mesha <mesha@KATIJA>
2026-01-04 21:32:08 +0100

    dd

--- sites/passwort_vergessen.php
+++ sites/passwort_vergessen.php
@@ -22,6 +22,7 @@ if (isset($_POST['reset'])) {
 <html lang="de">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Passwort zurücksetzen</title>
 <link rel="stylesheet" href="../css/style.css"> <!-- optional -->
 </head>
